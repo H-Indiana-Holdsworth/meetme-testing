de250543ec2d5b83836f37be2ffa777c3b6a7973
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-01-24 15:20:11 -0700

    Passing header test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,16 @@
+import { screen, render, waitForElementToBeRemoved } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from './App'
 
-test('Should render the header', () => {
+test('Should render the header', async () => {
+  render(<App />)
 
+  const meet = screen.getByText(/meet/i)
+  expect(meet).toBeInTheDocument()
+
+  const name = await screen.findByText(/vonta/i)
+  expect(name).toBeInTheDocument()
+
+  const img = await screen.findByAltText(/alchemy/i)
+  expect(img).toBeInTheDocument()
 })
